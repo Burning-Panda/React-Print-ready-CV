2e17208d398792ddc46362d5966cfba0b5f4b571
Burning-Panda <31665764+Burning-Panda@users.noreply.github.com>
2020-06-11 00:13:11 +0200

    Update design and files.
    Updated Project card, added icons.

--- src/cards/projectCard.js
+++ src/cards/projectCard.js
@@ -1,7 +1,9 @@
 import React from "react";
+import returnIcon from "./returnIcon";
+
 
 const projects = (prod) => {
-    console.log(prod)
+
     return (
         <div className="projects">
 
@@ -10,14 +12,37 @@ const projects = (prod) => {
                 <p>Mine Prosjekter</p>
             </div>
             <div className="projectsList">
-                <article className="project">
-                    <h2>Article</h2>
-                    <p>Text</p>
+                {prod.map((object,i) =>
+                <article className="project" key={i}>
+                    <div className="projectImage">
+                        <img src={ require('../assets/images/'+object.image+object.imageType) }  alt={object.imageAlt}/>
+                    </div>
+                    {(typeof object.url != "undefined") ? (
+                        <div><h3><a href={object.url}>{object.name}</a></h3><small>{object.url}</small></div>
+                    ) : (<h3 className="no-link">{object.name}</h3>)}
+
+                    <p>{object.description}</p>
+
+                    <div className="projectTools">
+                        {(typeof object.tools != "undefined") ? (
+                            <ul className="projProgList">
+                                {object.tools.map((item, j) =>
+                                    <li key={j}>{returnIcon(item)}</li>
+                                )}
+                            </ul>
+                        ) : ('')}
+                    </div>
                 </article>
-                <article className="project">
-                    <h2>Article</h2>
-                    <p>Text</p>
-                </article>
+                )}
+            </div>
+
+            <div className="Attribution">
+                <span>HTML5 Logo by<a href="https://commons.wikimedia.org/wiki/File:Devicon-html5-plain-wordmark.svg" title="via Wikimedia Commons">Julien Monty ( https://github.com/konpa ), author of Devicon repository, created this SVG file</a> / <a href="http://opensource.org/licenses/mit-license.php">MIT</a></span>
+                <span>CSS3 Logo by <a href="https://commons.wikimedia.org/wiki/File:CSS3_logo_and_wordmark.svg" title="via Wikimedia Commons">Rudloff</a> / <a href="https://creativecommons.org/licenses/by/3.0">CC BY</a></span>
+                <span><a href="https://commons.wikimedia.org/wiki/File:Adobe_Illustrator_CC_icon.svg" title="via Wikimedia Commons">Adobe Systems</a> / Public domain</span>
+                <span><a href="https://commons.wikimedia.org/wiki/File:Adobe_Photoshop_CC_icon.svg" title="via Wikimedia Commons">Fred the Oyster</a> / Public domain</span>
+                <span><a href="https://commons.wikimedia.org/wiki/File:Python.svg" title="via Wikimedia Commons">The people from the Tango! project.</a> / <a href="https://creativecommons.org/licenses/by-sa/3.0">CC BY-SA</a></span>
+                <span><a href="https://commons.wikimedia.org/wiki/File:Javascript-shield.svg" title="via Wikimedia Commons">Omed Habib</a> / Public domain</span>
             </div>
         </div>
     )
--- src/cards/returnIcon.js
+++ src/cards/returnIcon.js
@@ -1,5 +1,13 @@
-import {ChatBubble, Group, InsertLink, Instagram, LinkedIn, VerifiedUser, GitHub} from "@material-ui/icons";
+import {ChatBubble, Group, InsertLink, Instagram, LinkedIn, VerifiedUser, GitHub, Facebook } from "@material-ui/icons";
 import React from "react";
+import AdobeXD from "../assets/svg/Adobe_XD_CC_icon.svg";
+import Illustrator from "../assets/svg/Adobe_Illustrator_CC_icon.svg";
+import Photoshop from "../assets/svg/Adobe_Photoshop_CC_icon.svg";
+import HTML from "../assets/svg/Devicon-html5-plain-wordmark.svg";
+import CSS from "../assets/svg/CSS3_logo_and_wordmark.svg";
+import Python from "../assets/svg/Python.svg";
+import Javascript from "../assets/svg/Javascript-shield.svg";
+// import CSS from "../assets/svg/";
 
 function returnIcon(ico) {
     switch (ico) {
@@ -17,6 +25,24 @@ function returnIcon(ico) {
             return (<LinkedIn />)
         case 'git':
             return (<GitHub />)
+        case 'fb':
+            return (<Facebook />)
+
+        case 'Adobe XD':
+            return (<img src={AdobeXD} alt="Adobe CC XD Ikon"/>)
+        case 'Adobe Illustrator':
+            return(<img src={Illustrator}  alt="Adobe CC Illustrator Ikon"/>)
+        case 'Adobe Photoshop':
+            return(<img src={Photoshop}  alt="Adobe CC Photoshop Ikon"/>)
+        case 'HTML':
+            return(<img src={HTML}  alt="HTML5 Logo by W3C" className="white-svg" />)
+        case 'CSS':
+            return(<img src={CSS}  alt="CSS3 Logo by W3C" className="white-svg" />)
+        case 'Javascript':
+            return (<img src={Javascript} alt="Javascript Ikon"/>)
+        case 'Python':
+            return (<img src={Python} alt="Python Ikon"/>)
+
         default:
             return null
     }
